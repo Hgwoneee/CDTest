77d711fcdc6925b7d8bab3efbca29bfe1ca518c2
Park HyeongGwon <gudrnjs0503@naver.com>
2024-03-21 14:59:40 +0900

    프로그래머스 LV1

---
+++ LV1 두 정수 사이의 합.js
@@ -0,0 +1,19 @@
+// 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+// 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+// a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+
+function solution(a, b) {
+    var answer = 0;
+    if(a < b) {
+        for(let i = a; i <= b; i++) {
+        answer += i
+        }
+    } else if (a > b) {
+        for(let i = b; i <= a; i++) {
+        answer += i
+        }
+    } else {
+        answer = a
+    }
+    return answer;
+}
